1acd006c76606ddbd5e1f9743e263cf3a335041f
Sebastian Prentice <prentice.seb@hotmail.com>
2024-03-23 03:12:23 +0000

    update more app_logger

--- app/cal/views.py
+++ app/cal/views.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends
 from app.auth.views import get_token
 from app.cal.tasks import create_cal_for_property, create_trips_from_ics
 from app.models import PropertyCal
+from app.cal._utils import app_logger
 
 cal_router = APIRouter()
 
@@ -13,6 +14,7 @@ cal_router = APIRouter()
 @cal_router.get('/get_property_cal')
 def get_property_cal(property_ref: str, token: str = Depends(get_token)):
     cal_link = create_cal_for_property(property_ref)
+    app_logger.info(property_ref)
     # add all cal stuff
     return {'propertyRef': property_ref, 'cal_link': cal_link}
 
@@ -20,8 +22,8 @@ def get_property_cal(property_ref: str, token: str = Depends(get_token)):
 # Sync External Calendar
 @cal_router.post('/cal_to_property')
 def cal_to_property(data: PropertyCal, token: str = Depends(get_token)):
-    logging.info(data.property_ref)
-    logging.info(data.cal_link)
+    app_logger.info(data.property_ref)
+    app_logger.info(data.cal_link)
     # add all cal stuff
     if create_trips_from_ics(data.property_ref, data.cal_link):
         return {'propertyRef': data.property_ref, 'message': 'Calendar successfully added to property'}
