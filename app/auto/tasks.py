5eabd62ddd740a1d5d7e0a2404d0f01be58c086a
Sebastian Prentice <prentice.seb@hotmail.com>
2024-04-08 02:41:14 +0100

    ahhhhhhhhhh

--- app/auto/tasks.py
+++ app/auto/tasks.py
@@ -133,7 +133,7 @@ def auto_complete_and_notify():
                             except Exception as e:
                                 app_logger.error('Failed to update trip %s for property %S: %s', trip.id, prop.id, e)
 
-                        if 'tripBeginDateTime' in trip and trip.get('upcoming'):
+                        if 'tripBeginDateTime' in trip.to_dict() and trip.get('upcoming'):
                             time_difference = trip.get('tripBeginDateTime') - current_time
 
                             with logfire.span(f'Time till trip starts: {time_difference}'):
