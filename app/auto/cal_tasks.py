249f5bbf5013a9ae49b08ee7127eaf58832ee7ab
Sebastian Prentice <prentice.seb@hotmail.com>
2024-09-01 18:39:25 +0100

    always renew

--- app/auto/cal_tasks.py
+++ app/auto/cal_tasks.py
@@ -16,32 +16,21 @@ def auto_check_and_renew_channels():
         # Iterate over all property documents
         properties_ref = db.collection('properties').stream()
         for prop in properties_ref:
-            # Get the channel expiration time
+            app_logger.info('Renewing channel for property: %s', prop.id)
+            calendar_id = prop.get('externalCalendar')
+            channel_id = prop.get('channelId')
+            channel_address = settings.url + calendar_id
             try:
-                channel_expiration = prop.get('channelExpiration')
-            except KeyError:
-                app_logger.info('Channel expiration time not found for property: %s', prop.id)
+                new_channel, expiration = renew_notification_channel(calendar_id, channel_id, 'web_hook', channel_address)
+            except Exception as e:
+                app_logger.error('Error renewing channel for property: %s', prop.id)
+                app_logger.error(e)
                 continue
 
-            # If the channel is about to expire, renew it
-            if channel_expiration and datetime.fromtimestamp(int(channel_expiration) / 1000) - now < timedelta(days=2):
-                app_logger.info('Renewing channel for property: %s', prop.id)
-                calendar_id = prop.get('externalCalendar')
-                channel_id = prop.get('channelId')
-                channel_address = settings.url + calendar_id
-                try:
-                    new_channel, expiration = renew_notification_channel(calendar_id, channel_id, 'web_hook', channel_address)
-                except Exception as e:
-                    app_logger.error('Error renewing channel for property: %s', prop.id)
-                    app_logger.error(e)
-                    continue
+            # Update the property document with the new channel id and expiration time
+            prop.reference.update({'channelId': new_channel['id'], 'channelExpiration': expiration})
 
-                # Update the property document with the new channel id and expiration time
-                prop.reference.update({'channelId': new_channel['id'], 'channelExpiration': expiration})
-
-                app_logger.info('Channel for property: %s successfully renewed', prop.id)
-            else:
-                app_logger.info('Channel for property: %s does not need to be renewed', prop.id)
+            app_logger.info('Channel for property: %s successfully renewed', prop.id)
 
 def resync_all_calendar_events():
     """
