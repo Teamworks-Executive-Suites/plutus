e39aeddeb6afd5c0e8a97abbe0b873787fdb2005
Sebastian Prentice <prentice.seb@hotmail.com>
2024-09-01 19:10:29 +0100

    idk whats happening

--- app/main.py
+++ app/main.py
@@ -20,15 +20,11 @@ from app.utils import settings
 async def lifespan(app: FastAPI):
     logging.info('startup')
 
-    auto_check_and_renew_channels()
-    auto_complete_and_notify()
-
     scheduler = BackgroundScheduler()
     scheduler.add_job(auto_check_and_renew_channels, 'interval', hours=1)
     scheduler.add_job(auto_complete_and_notify, 'interval', hours=1)
     scheduler.start()
     logging.info('Scheduler initialized and started')
-    logging.info('Job auto_check_and_renew_channels added to scheduler')
 
     yield
 
